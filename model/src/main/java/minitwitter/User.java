73a51cf4c1c53cdf7b45e9251c3dd0c0ed89241a
deepapanwar <dppnwr3@gmail.com>
2015-08-27 20:49:03 +0530

    updated user class

--- model/src/main/java/minitwitter/User.java
+++ model/src/main/java/minitwitter/User.java
@@ -1,10 +1,31 @@
 package minitwitter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 /**
  * Created by deepap on 27/8/15.
  */
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+
+    protected User() {}
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+        @Override
+    public String toString() {
+        return String.format(
+                "User[id=%d, name='%s', email= '%s']",
+                id, name, email);
+    }
 }
