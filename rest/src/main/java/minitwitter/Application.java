4f17b43ce2a33de354347c3b020117437fbe47c4
deepapanwar <dppnwr3@gmail.com>
2015-08-27 16:57:14 +0530

    dummy link added for getting  tweets and tweetId

--- rest/src/main/java/minitwitter/Application.java
+++ rest/src/main/java/minitwitter/Application.java
@@ -29,4 +29,16 @@ class MiniTwitterAPIController {
     String getUserProfile(@PathVariable String userId) {
         return "User profile for User " + userId;
     }
+
+
+    @RequestMapping(value="/users/{userId}/tweets", method = RequestMethod.GET)
+    String getUserTweets(@PathVariable String userId) {
+        return "Listing tweets for user " + userId;
+    }
+
+    @RequestMapping(value= "/tweets/{tweetId}", method = RequestMethod.GET)
+    String getTweet(@PathVariable String tweetId) {
+        return "tweet for tweetId " +  tweetId;
+    }
 }
+
