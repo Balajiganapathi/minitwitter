e738d7469436cdc9c6266a9bbe3c4fbd1bbcc31d
Balajiganapathi S <balajiganapathi.s@gmail.com>
2015-09-14 19:57:00 +0530

    Implemented follow/unfollow link on profile page.

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -22,6 +22,36 @@ function getFollowee(userId) {
     };
     $.ajax(query);
 }
+
+function toggleFollow(curId, userId) {
+    var f = $("#followLink").attr("value");
+    var query = {
+        url: "http://localhost:8080/api/users/follow/" + userId + "?sessionId=" + Cookies.get("sessionId"),
+        type: (f == "follow"? "POST": "DELETE"),
+        data: "",
+        dataType: "text",
+        cache: false,
+        success: function(json) {
+            console.log(json);
+            console.log(f + "ed successfully");
+            if(f == "follow") {
+                $("#followLink").attr("value", "unfollow");
+            } else {
+                $("#followLink").attr("value", "follow");
+            }
+        },
+        error: function(xhr, status, errorThrown){
+            console.log("error");
+            $("#error").text("Some error occurred (" + errorThrown + ")");
+        },
+        complete: function( xhr, status ) {
+            console.log( "The request is complete!" );
+        }
+    };
+
+    $.ajax(query);
+}
+
 function getFollowers(userId) {
     var query = {
         url : "http://localhost:8080/api/users/"+userId+"/followers",
@@ -34,6 +64,63 @@ function getFollowers(userId) {
             data = $.parseJSON(json);
             console.log(data);
             $('#followers').text(data.length + " followers");
+            if(!Cookies.get("sessionId")) {
+                $("#followLink").hide();
+                return;
+            }
+
+            function determineStatus(json) {
+                console.log("Status: " + json);
+                console.log($.parseJSON(json));
+                var curId = $.parseJSON(json).id;
+                console.log("c/u: " + curId + " " + userId);
+                if(curId == userId) {
+                    $("#followLink").hide();
+                    return;
+                }
+
+                var following = false;
+                console.log("Loop: " + data.length);
+                for(var i = 0; i < data.length; i++) {
+                    var f = data[i];
+                    console.log(f);
+                    console.log(f.follower);
+                    console.log(f.follower.id);
+                    if(f.follower.id == curId) {
+                        following = true;
+                    }
+                }
+
+                if(following) {
+                    $("#followLink").attr("value", "unfollow");
+                } else {
+                    $("#followLink").attr("value", "follow");
+                }
+
+                $("#followLink").on('click', function(event) {
+                    toggleFollow(curId, userId);
+                    event.preventDefault();
+                });
+
+            }
+
+            var userquery = {
+                    url : "http://localhost:8080/api/users/profile?sessionId=" + Cookies.get("sessionId"),
+                    type : "GET",
+                    data : "",
+                    dataType : "text",
+                    success : determineStatus,
+                    error: function(xhr, status, errorThrown){
+                         console.log("error");
+                         $("#error").text("Some error occurred (" + errorThrown + ")");
+                     },
+                     complete: function( xhr, status ) {
+                         console.log( "The request is complete!" );
+                     }
+            }
+            console.log(userquery);
+            $.ajax(userquery);
+
         },
         error: function(xhr, status, errorThrown){
              console.log("error");
