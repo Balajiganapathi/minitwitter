1e94f598e4dddf4621a1942c1323af4c20e012f3
Balajiganapathi S <balajiganapathi.s@gmail.com>
2015-09-14 14:47:11 +0530

    Sets cookie on login.

--- src/main/resources/static/js/js_login.js
+++ src/main/resources/static/js/js_login.js
@@ -7,21 +7,28 @@ $(document).ready(function(){
             dataType: "text",
             cache: false,
             success: function(json) {
-                alert("logged in successfully");
                 console.log(json);
+                data = $.parseJSON(json);
+                console.log(data);
+                console.log(data["sessionId"]);
+                Cookies.set("sessionId", data["sessionId"]);
+                console.log("logged in successfully");
             },
             error: function(xhr, status, errorThrown){
                 console.log("error");
+                $("#error").text("Some error occurred (" + errorThrown + ")");
             },
             complete: function( xhr, status ) {
                 console.log( "The request is complete!" );
             }
         };
+
         console.log(query);
         $.ajax(query);
         console.log("Request sent!");
         event.preventDefault();
     };
+
     $('#login').on('click', login);
     console.log("Ready!");
 });
