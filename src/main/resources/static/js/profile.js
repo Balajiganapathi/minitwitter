2c8b2d53c2db8ff1846fc404863e946f7d04ed9f
deepapanwar <dppnwr3@gmail.com>
2015-09-14 18:29:10 +0530

    added follower and followee to profile page

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,3 +1,51 @@
+function getFollowee(userId) {
+    var query = {
+        url : "http://localhost:8080/api/users/"+userId+"/followee",
+        type : "GET",
+        data : "",
+        dataType : "text",
+        cache:false,
+        success : function(json){
+            console.log(json);
+            data = $.parseJSON(json);
+            console.log(data);
+            $('#following').text("following " + data.length);
+        },
+        error: function(xhr, status, errorThrown){
+             console.log("error");
+             $("#error").text("Some error occurred (" + errorThrown + ")");
+         },
+         complete: function( xhr, status ) {
+             console.log( "The request is complete!" );
+         }
+
+    };
+    $.ajax(query);
+}
+function getFollowers(userId) {
+    var query = {
+        url : "http://localhost:8080/api/users/"+userId+"/followers",
+        type : "GET",
+        data : "",
+        dataType : "text",
+        cache:false,
+        success : function(json){
+            console.log(json);
+            data = $.parseJSON(json);
+            console.log(data);
+            $('#followers').text(data.length + " followers");
+        },
+        error: function(xhr, status, errorThrown){
+             console.log("error");
+             $("#error").text("Some error occurred (" + errorThrown + ")");
+         },
+         complete: function( xhr, status ) {
+             console.log( "The request is complete!" );
+         }
+
+    };
+    $.ajax(query);
+}
 $(document).ready(function() {
         var ul = window.location.href;
         var index = ul.split('/');
@@ -13,6 +61,8 @@ $(document).ready(function() {
                     data = $.parseJSON(json);
                     $('#name').text(data.name);
                     $('#email').text(data.email);
+                    getFollowers(userId);
+                    getFollowee(userId);
             },
             error: function(xhr, status, errorThrown){
                 console.log("error");
